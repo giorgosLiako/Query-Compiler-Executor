7063a7750826e63fc1670221f155834b4c2520d4
J1635 <orestisg@me.com>
2019-11-01 20:44:37 +0200

    fixed sort

--- main/sort_main.c
+++ main/sort_main.c
@@ -14,35 +14,35 @@ int main() {
     relation S;
 
 
-    R.num_tuples = 10;
+    R.num_tuples = 1000000;
     R.tuples = MALLOC(tuple, R.num_tuples);
 
-    S.num_tuples = 5;
+    S.num_tuples = 1000000;
     S.tuples = MALLOC(tuple, S.num_tuples);
 
     srand(32839832);
 
   //  log_info("ARRAY: KEY\tROW_ID\n");
 
-        R.tuples[0].key = 1;
-        R.tuples[0].payload = 0;
+        // R.tuples[0].key = 1;
+        // R.tuples[0].payload = 0;
 
-        R.tuples[1].key = 1;
-        R.tuples[1].payload = 1;
+        // R.tuples[1].key = 1;
+        // R.tuples[1].payload = 1;
 
-        S.tuples[0].key = 1;
-        S.tuples[0].payload = 0;
+        // S.tuples[0].key = 1;
+        // S.tuples[0].payload = 0;
 
-        S.tuples[1].key = 1;
-        S.tuples[1].payload = 1;
+        // S.tuples[1].key = 1;
+        // S.tuples[1].payload = 1;
 
     for (size_t i = 2 ; i < R.num_tuples ; i++) {
-        // uint64_t key = 0;
-        // for (int j=0; j<64; j++) {
-        //     key = key*2 + rand()%2;
-        // }
+        uint64_t key = 0;
+        for (int j=0; j<64; j++) {
+            key = key*2 + rand()%2;
+        }
         
-        uint64_t key = rand()%10;
+        // uint64_t key = rand()%10;
         R.tuples[i].key = key;
         R.tuples[i].payload = i;
 
@@ -50,12 +50,12 @@ int main() {
     }
 
     for (size_t i = 2 ; i < S.num_tuples ; i++) {
-        // uint64_t key = 0;
-        // for (int j=0; j<64; j++) {
-        //     key = key*2 + rand()%2;
-        // }
+        uint64_t key = 0;
+        for (int j=0; j<64; j++) {
+            key = key*2 + rand()%2;
+        }
         
-        uint64_t key = rand()%10;
+        // uint64_t key = rand()%10;
         S.tuples[i].key = key;
         S.tuples[i].payload = i;
 
