c6a67329e363d50cf9f4f833a96b561f49854090
J1635 <orestisg@me.com>
2020-01-12 17:19:33 +0200

    we need to complete cost to test it

--- src/join_enumeration.c
+++ src/join_enumeration.c
@@ -2,131 +2,143 @@
 #include "queries.h"
 #include "DArray.h"
 #include "join.h"
+#include "best_tree.h"
+#include <stdarg.h>
+#include "set.h"
 
 
 #define RightDeepOnly 1
 #define LeftDeepOnly 0
 
-int compare_relation_column(relation_column r1, relation_column r2) {
-    if (r1.column != r2.column) return 0;
-    if (r1.relation != r2.relation) return 0;
-    return 1;
+//works for numbers smaller than 10
+char *to_string(int rel) {
+    char *s = (char *) malloc(sizeof(char)*2);
+    sprintf(s, "%d", rel);
+    return s;
 }
 
-typedef struct tree{
-    void *right;
-    void *left;
-} tree;
+char *copy_string(char* str) {
+    char *s = (char *) malloc(strlen(str)*1);
+    strcpy(s, str);
+    return s;
+}
 
-typedef struct best_tree_node {
-    DArray *key;
-    tree *value;
-    struct best_tree_node *next;
-} best_tree_node;
-
-typedef struct {
-    best_tree_node *start;
-    best_tree_node *end;
-} best_tree_struct;
-
-best_tree_struct *current_best_tree;
-
-int sort_relation_column(relation_column *r_c1, relation_column *r_c2) {
-    if (r_c1->relation > r_c2->relation) return 1;
-    if (r_c1->column > r_c2->column) return 1;
+int in_string(char* str, int rel) {
+    char *rel_str = to_string(rel);
+    int size = strlen(str);
+    for (size_t i = 0; i < size; i++) {
+        if (str[i] ==  rel_str[0]) return 1;
+    }
+    
+    free(rel_str);
     return 0;
 }
 
-//T2 can be a tree
-tree *create_join_tree(tree *T1, relation_column *T2) {
+char *append_string(int rel, char* str) {
+    char* new = (char *) malloc(sizeof(char)*(strlen(str) + 2));
+    char* rel_str = to_string(rel);
+
+    int str_index = 0, new_index = 0;
+    for (size_t i = 0; i < strlen(str) + 1; i++) {
+        if (str[str_index] > rel_str[0]){
+            new[new_index++] = rel_str[0];
+        } else {
+            new[new_index++] = str[str_index++];
+        }
+    }
+    new[str_index] = '\0';
+
+    free(rel_str);
+    return new;
+    
+}
+
+tree *to_tree(int rel){
+    tree *new;
+    new = (tree*) malloc(sizeof(tree));
+    new->left = NULL;
+    new->right = NULL;
+    new->type = 1;
+    new->rel = rel;
+
+    return new;
+}
+
+tree *create_join_tree(tree *t, int rel) {
     tree *best_tree = (tree*) malloc(sizeof(tree));
-    if (!RightDeepOnly) {
-        tree *test_tree;
-        test_tree->left = T1;
-        test_tree->right = T2;
-        if (best_tree == NULL || cost(best_tree) > cost(test_tree)){
-            best_tree->left = test_tree->left;
-            best_tree->right = test_tree->right;
-        }
-    }
-    if (!LeftDeepOnly) {
-        tree *test_tree;
-        test_tree->left = T2;
-        test_tree->right = T1;
-        if (best_tree == NULL || cost(best_tree) > cost(test_tree)){
-            best_tree->left = test_tree->left;
-            best_tree->right = test_tree->right;
-        }
-    }
+    best_tree->type = 0;
+    best_tree->left = t;
+    best_tree->right = to_tree(rel);
+    
+    
     return best_tree;
 }
 
-DArray *to_set(relation_column r_l) {
-    DArray *new_set = DArray_create(sizeof(relation_column*), 1);
-    DArray_push(new_set, &r_l);
-    return new_set;
-}
+int *predicate_graph(predicate *preds, int pred_size, int rel_size) {
+    int **graph = (int**) malloc(rel_size*sizeof(int*));
+    for (size_t i = 0; i < rel_size; i++) {
+        graph[i] = (int*) malloc(rel_size*sizeof(int));
 
-void init_best_tree(){
-    current_best_tree = (best_tree_struct*) malloc(sizeof(best_tree_struct));
-}
-
-
-
-
-
-
-
-tree* BestTree(void*);
-tree* to_tree(relation_column);
-
-
-
-DArray *create_set(relation_column *r_c, int r_c_size, int size){
-    DArray *new_set = DArray_create(sizeof(DArray*), 1);
-    for (size_t i = 0; i < r_c_size - 1; i++) {
-        DArray *new_sub_set = DArray_create(sizeof(relation_column*), 1);
-        DArray_push(new_sub_set, &r_c[i]);
-        for (size_t j = i + 1; j < size; j++) {
-            DArray_push(new_sub_set, &r_c[j]);
-        }
-        DArray_push(new_set, &new_sub_set);
+        for (size_t j = 0; j < rel_size; j++) graph[i][j] = 0;
     }
-    return new_set;
+
+    for (size_t i = 0; i < pred_size; i++) {
+        int index_1 = preds[i].first.relation;
+        int index_2 = (*(relation_column *) preds[i].second).relation;
+
+        graph[index_1][index_2] = 1;
+        graph[index_2][index_1] = 1;
+    }
+    
+    return graph;
 }
 
-int in_set(DArray *set, relation_column r_c) {
-    for (size_t i = 0; i < DArray_count(set); i++) {
-        if (compare_relation_column((*(relation_column*) DArray_get(set, i) ), r_c)){
-            return 1;
-        }
-    }
+int connected(int **graph, char *str, int rel) {
+    for (size_t i = 0; i < strlen(str); i++) 
+        if (graph[str[i] - '0'][rel]) return 1;
     return 0;
 }
 
-void set_BestTree(DArray * set, tree* tr){
-    DArray_sort(set, sort_relation_column);
-    //TODO
-    
-}
-int cost(tree*);
 
-tree *dp_linear(relation_column *relation_column, int size){
-    for (size_t i = 0; i < size; i++) {
-        setBestTree(to_set(relation_column[i]), to_tree(relation_column[i]));
+void dfs(tree *t, relation *relations, predicate *predicates, int *res) {
+    if (t->left->type == 0) dfs(t->left, relations, predicates, res);
+    //TODO: calculate the f value
+    //res += t->left->rel
+}
+
+int cost(tree *t, relation *relations, predicate *predicates) {
+    int res = 0;
+    dfs(t, relations, predicates, &res);
+    return res;
+}
+
+
+tree *dp_linear(relation *relations, int rel_size, predicate *predicates, int pred_size){
+    queue *set = new_queue();
+    int **g = predicate_graph(predicates, pred_size, rel_size);
+
+    for (size_t i = 0; i < rel_size; i++) {
+        char* n_set = to_string(i);
+        BestTree_set(n_set, to_tree(i));
+        push(set, copy_string(n_set));
     }
 
-    for (size_t i = 0; i < size; i++) {
-        DArray *set = create_set(relation_column, size, i);
-        for (size_t j = 0; j < DArray_count(set); j++) {
-            for (size_t k = 0; k < size; k++) {
-                if (!in_set(set, relation_column[i])) {
-                    tree *curr_tree = create_join_tree(BestTree(set), relation_column[i]);
+    for (size_t i = 0; i < rel_size - 1; i++) {
+        int set_size = q_size(set);
+        char* old_set = pop(set);
+
+        for (size_t j = 0; j < set_size; j++) {
+
+            for (size_t k = i + 1; k < rel_size; k++) {
                 
-                    DArray *new_set = set;
-                    DArray_push(new_set, &relation_column[i]);
-                    if (BestTree(new_set) == NULL || cost(BestTree(new_set))>cost(curr_tree) ){
-                        set_BestTree(new_set, curr_tree);
+                if (!in_string(old_set, k) && connected(g, old_set, k)) {
+                    tree *curr_tree = create_join_tree(BestTree(old_set), k);
+                    char *new_set = append_string(k, old_set);
+
+                    //TODO cost
+                    if (BestTree(new_set) == NULL || cost(BestTree(new_set), relations, predicates) > cost(curr_tree, relations, predicates) ){
+                        if (BestTree(new_set) == NULL) push(set, copy_string(new_set));
+                        BestTree_set(new_set, curr_tree);
                     } 
                 
                 }
@@ -135,6 +147,7 @@ tree *dp_linear(relation_column *relation_column, int size){
         }
         
     }
-    return BestTree(create_set(relation_column, size, size));
+    return BestTree(pop(set));
     
 }
+//&& connected(set, relations[k])
